ac6d3bfb7c4dbc9c27b5f0d3b4ddeb4abb579a73
XinruiGUO <xinrui.guo@ensta-bretagne.org>
2015-05-13 22:11:10 +0200

    change all the code

--- code/ast.rb
+++ code/ast.rb
@@ -1,33 +1,37 @@
 class Ast
   def accept(visitor, arg=nil)
     name = self.class.name.split(/::/)[0]
-    #puts "- visiting#{name}"
     visitor.send("visit#{name}".to_sym, self ,arg) #Jingle! Metaprog !
   end
 end
 
-
-
 class Identifier < Ast
-  attr_accessor :val,:selector
-  def initialize val
-    @val=val
+  attr_accessor :name
+  def initialize name=nil
+    @name=name
   end
 
   def to_s
-    @val
+    @name.value
   end
 end
 
 # Jingle! exactly similar to Identifier !
 # this could be refactored to Number < Data & Identifier < Data
-class Number < Ast 
-  attr_accessor :val
-  def initialize val
-    @val=val
-  end
-  def to_s
-    @val.to_s
+# class Number < Ast 
+#   attr_accessor :val
+#   def initialize val
+#     @val=val
+#   end
+#   def to_s
+#     @val.to_s
+#   end
+# end
+
+class FieldList < Ast
+  attr_accessor :identList,:type
+  def initialize identList=[],type=nil
+    @identList,@type = identList, type
   end
 end
 
@@ -35,94 +39,238 @@ class Stmt < Ast
 end
 
 class Modul < Ast
-  attr_accessor :ident,:declarations,:statements
-  def initialize ident=nil,declarations=[],statements=[]
-    @ident,@declarations,@statements=ident,declarations,statements
+  attr_accessor :ident1,:ident2,:declarations,:statements
+  def initialize ident1=nil,ident2=nil,declarations=nil,statements=nil
+    @ident1,@ident2,@declarations,@statements  =  ident1,ident2,declarations,statements
+  end
+end
+#=============== declarations rule====================
+class Declarations < Ast
+  attr_accessor :consts,:types,:vars,:procs
+  def initialize consts=[],types=[],vars=[],procs=[]
+    @consts,@types,@vars,@procs=consts,types,vars,procs
   end
 end
 
-class Const < Ast
+class TypeDeclarations < Ast
+  attr_accessor :list
+  def initialize list=[]
+    @list = list
+  end
+end
+
+class TypeDecl < Ast
+  attr_accessor :ident,:type
+  def initialize ident,type
+    @ident,@type=ident;type
+  end
+end
+
+class ConstDeclarations < Ast
+  attr_accessor :list
+  def initialize list=[]
+    @list = list
+  end
+end
+
+class ConstDecl < Ast
   attr_accessor :ident,:expr
   def initialize ident=nil,expr=nil
     @ident,@expr=ident,expr
   end
 end
 
-class Type < Ast
-  attr_accessor :ident,:type
-  def initialize ident=nil,type=nil
-    @ident,@type=ident,type
+class VarDeclarations < Ast
+  attr_accessor :list
+  def initialize varDecls=[]
+    @list = varDecls
   end
 end
 
-class ArrayType
-  attr_accessor :size,:type
-  def initialize size=0,type=nil
-    @size,@type=size,type
-  end
-end
-
-class RecordType
-  attr_accessor :fieldList
-  def initialize fieldList=[]
-    @fieldList=fieldList
-  end
-end
-
-class FieldSection
+class VarDecl < Ast
   attr_accessor :identList,:type
-  def initialize identList=[],type=nil
-    @identList,@type=identList,type
+  def initialize identList=nil
+    @identList = identList
   end
 end
 
-class Var < Ast
-  attr_accessor :ident,:type
-  def initialize ident=nil,type=nil
-    @ident,@type=ident,type
+class ProcedureDeclarations < Ast
+  attr_accessor :list
+  def initialize list=[]
+    @list = list
   end
-end
 
-class ProcedureCall < Stmt
-  attr_accessor :name,:actualParams
-  def initialize name=nil,actualParams=[]
-    @name,@actualParams=name,actualParams
+  def size
+    @list.size
   end
+
 end
 
-class ProcedureHeading < Stmt
-  attr_accessor :name,:formalParameters
-  def initialize name=nil,formalParameters=[]
-    @name,@formalParameters=name,formalParameters
-  end
-end
-
-class FormalParameter
-  attr_accessor :identList,:type,:isVar
-  def initialize identList=[],type=nil,isVar=false
-    @identList,@type,@isVar=identList,type,isVar
-  end
-end
-
-class ProcedureBody < Stmt
-  attr_accessor :decls,:stmts
-  def initialize decls=[],stmts=[]
-    @decls,@stmts=decls,stmts
-  end
-end
-
-class Declaration < Ast
-end
-
-class Procedure < Declaration
+class ProcedureDecl < Ast
   attr_accessor :heading,:body
   def initialize heading=nil,body=nil
     @heading,@body=heading,body
   end
 end
 
-class Assign < Stmt
-  attr_accessor :assignee,:expression
+#================= types ===============
+#added jcll
+
+class Type < Ast
+end
+
+class NamedType < Type
+  attr_accessor :name
+  def initialize name
+    @name=name
+  end
+end
+
+class ArrayType < Type
+  attr_accessor :size,:type
+  def initialize size=0,type=nil
+    @size,@type=size,type
+  end
+end
+
+class RecordType < Type
+  attr_accessor :fieldLists
+  def initialize fieldLists=[]
+    @fieldLists=fieldLists
+  end
+end
+#================= types END ===============
+
+class IdentList < Ast
+  attr_accessor :idents
+  def initialize idents=[]
+    @idents=idents
+  end
+
+  def each &block
+    idents.each(&block)
+  end
+end
+
+
+class ActualParameters < Ast
+  attr_accessor :expressions
+  def initialize expressions=[]
+    @expressions=expressions
+  end
+end
+
+#=============== factor =================
+class Factor < Ast
+end
+
+class Number < Factor
+  attr_accessor :number
+  def initialize number=nil
+    @number = number
+  end
+
+  def to_s
+    @number.value.to_s
+  end
+end
+
+class ExprFactor < Factor
+  attr_accessor :expr
+  def initialize expr=nil
+    @expr = expr
+  end
+end
+
+class TildeFactor < Factor
+  attr_accessor :expr
+  def initialize expr=nil
+    @expr = expr
+  end
+end
+
+#=========== end factor ================
+
+class Term < Ast
+  attr_accessor :lhs, :op, :rhs
+  def initialize lhs=nil, op = nil, rhs=nil
+    @lhs, @op, @rhs = lhs, op, rhs
+  end
+end
+
+class SignedTerm < Ast
+  attr_accessor :op,:term
+  def initialize op=nil,term=nil
+    @op,@term=op,term
+  end
+end
+
+class SimpleExpression < Ast
+  attr_accessor :terms
+  def initialize terms=[]
+    @terms = terms
+  end
+end
+
+class Value < Ast
+  attr_accessor :op, :term
+  def initialize op = nil, term = nil
+    @op, @term = op, term
+  end
+end
+
+class StatementSequence < Ast
+  attr_accessor :list
+  def initialize list=[]
+    @list = list
+  end
+
+  def each &block
+    list.each(&block)
+  end
+end
+
+class ProcedureCall < Stmt
+  attr_accessor :name,:actualParams
+  def initialize name=nil,actualParams=nil
+    @name,@actualParams=name,actualParams
+  end
+end
+
+class ProcedureHeading < Stmt
+  attr_accessor :name,:formalParameters
+  def initialize name=nil,formalParameters=nil
+    @name,@formalParameters=name,formalParameters
+  end
+end
+
+class FPSection < Ast
+  attr_accessor :identList,:type,:isVar
+  def initialize identList=[],type=nil,isVar=false
+    @identList,@type,@isVar=identList,type,isVar
+  end
+end
+
+class FormalParameters < Ast
+  attr_accessor :fpSections
+  def initialize fpSections=[]
+    @chain=chain
+  end
+end
+
+class ProcedureBody < Stmt
+   attr_accessor :decls,:stmts, :ident
+   def initialize decls=nil, stmts=nil, ident=nil
+     @decls,@stmts, @ident = decls ,stmts, ident
+   end
+end
+
+class Declaration < Ast
+end
+
+
+class Assignment < Stmt 
+  attr_accessor :ident,:selector, :expression
   def initialize assignee=nil,expression=nil
     @assignee,@expression=assignee,expression
   end
@@ -130,7 +278,7 @@ end
 
 class If < Stmt
   attr_accessor :cond,:ifBlock,:elseBlock
-  def initialize cond=nil,ifBlock=[],elseBlock=[]
+  def initialize cond=nil,ifBlock=nil,elseBlock=nil
     @cond,@ifBlock,@elseBlock=cond,ifBlock,elseBlock
   end
 end
@@ -149,10 +297,28 @@ class Expression < Ast
   end
 end
 
-class Selector
-  attr_accessor :ident,:expr
-  def initialize ident=nil,expr=nil
-    @ident,@expr=ident,expr
+class Selector < Ast
+  attr_accessor :chain
+  def initialize chain=[]
+    @chain=chain
+  end
+
+  def <<(e)
+    @chain << e
+  end
+end
+
+class IdentSelector < Ast
+  attr_accessor :ident,:selector
+  def initialize ident=nil,selector=nil
+    @ident,@selector=ident,selector
+  end
+end
+
+class TabSelector < Ast
+  attr_accessor :expr
+  def initialize expr=nil
+    @expr=expr
   end
 end
 
--- code/compiler.rb
+++ code/compiler.rb
@@ -1,27 +1,36 @@
 require 'pp'
 
+require_relative 'parser'
+require_relative 'visitor'
+require_relative 'dot_generator'
+
 class Compiler
 
   def initialize h={}
     puts "Oberon-0 compiler".center(40,'=')
-    pp h
-    case h[:parser]
-    when nil
-      require_relative 'parser'
-    when :eleves
-      require_relative 'parser_etudiant'
-    else
-      raise "unknown parser for OBERON compiler"
-    end
+    #pp h
     @parser=Parser.new
   end
 
   def compile filename
     raise "usage error : Oberon-0 file needed !" if not filename
     puts "==> compiling #{filename}"
-    ast=@parser.parse(filename)
+    @ast=@parser.parse(filename)
+    #pp ast
+    simpleVisit
+    generate_dot
   end
   
+  def simpleVisit
+    visitor=Visitor.new
+    visitor.doIt(@ast)
+  end
+
+  def generate_dot
+    gen=DotGenerator.new
+    gen.generate(@ast)
+  end
+
 end
 
 compiler=Compiler.new :parser=>:eleves
